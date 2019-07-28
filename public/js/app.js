a2842190d48ffb6afac5fec25c81e4b5ec11fd52
saravana042 <usaravanankarthick@gmail.com>
2019-07-28 20:19:55 +0800

    Remove unecessary console log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("load ed");
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
